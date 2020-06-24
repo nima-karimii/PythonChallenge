ef8408d0a0502268cb4bb794069d6a7a9674b367
nima-karimii <karimii@gmail.com>
2020-06-24 20:52:15 +0800

    First uploade of My Python

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,76 +1,127 @@
 
-import os
+# Please First run it in your Terminal , Thanks ;)
 
-import  csv
+
+import os
+import sys
+import csv
+import shutil
+from colorama import Fore, Back, Style 
+
+
+# CLearing The ScreenTerminal
 os.system('cls')
 
-csvpath = os.path.join('Resources','budget_data.csv')
+# Adreess of Source data to read and destination of genarating reports
+
+SavingRoot='analysis/report.txt'
+ReadingRootFile='Resources/budget_data.csv'
+
+# Opening and Reading the Data from Source
+
+csvpath = os.path.join(ReadingRootFile)
 
 with open (csvpath) as csvfile :
     csvreader = csv.reader(csvfile, delimiter=',')
-  #  print(csvreader)
     csv_header = next(csvreader)
-  #  print(f"CSV Header: {csv_header}")
-    Rowcount=0
-    Total=0
-    FirstValue =0
-    LastValue =0
-    MaxValue=0
-    MinValue=0
-    DateMax=""
-    DateMin=""
-    MaxChange=0
-    MinChange=0
-    MaxChangedate=""
-    MinChangedate=""
-    PerValue=0
+  
+    # Define the Variables
+    Rowcount=0               #RowCounter
+    Total=0                  #Sum up the Profit/Loose
+    FirstValue =0            #First Profit/Loose
+    FirstDay=""              #First Day of Profit/Loose
+    LastValue =0             # LAst Profit/Loose
+    LastDay=""               #Last Day of Profit/Loose
+    MaxValue=0               # MAx Profit/Loose
+    MinValue=0               # Min Profit/Loose
+    DateMax=""               # Date of Max Profit/Loose
+    DateMin=""               # DAte of Min Profit/Loose
+    MaxChange=0              # Maximum change in 2 Days
+    MinChange=0              # Minimum Change in 2 Days
+    MaxChangedate=""         # Day of Maximum Change
+    MinChangedate=""         # Day of Minimum Change
+    PerValue=0               # Previuse day Profit/Loose
 
 
 
     for row in csvreader:
 
-        print(row)
-
-
+       # Finding the first value and Last value for caculating the Avrage change
         if Rowcount== 0 :
             FirstValue = int(row[1])
+            FirstDay = row[0]    
         else:
             LastValue = int(row[1])
-
+            LastDay = row[0]
+            
+        # Finding the MAximum Profit and the date
         if int(row[1])> MaxValue :
             MaxValue= int(row[1])
             DateMax = row[0]
 
+        #Finding the Maximum Loose and the date
         if int(row[1])< MinValue :
             MinValue= int(row[1])
             DateMin = row[0]
 
+        # Finding the MAximum Profit change  and the date
         if int(row[1])-PerValue >MaxChange:
             MaxChange =int(row[1])-PerValue
             MaxChangedate = row[0]
 
+        # Finding the MAximum Loose change and the date
         if int(row[1])-PerValue < MinChange:
             MinChange =int(row[1])-PerValue
             MinChangedate = row[0]    
             
+        # For the Next Loop we need the The Value of the previous Profit/Loose to calculate the change 
         PerValue=int(row[1])
 
+        # Calculating the sum of Profit/Loose
         Total= int(row[1]) + Total
         
+        #Cunting the number of days
         Rowcount= Rowcount +1
     # print(row)
+
+#Calculating the Avrage of Profit/Loose  
 AvarageValue = Total/Rowcount
+
+#Calculating the Avrage change of Profit/Loose
 AvarageChange= (FirstValue-LastValue)/(1-Rowcount)
 
-print ('Total Months: ' + str(Rowcount))
-print ('Total: '+ str(Total))
-print('Average: '+str(AvarageValue))
-#print(str(FirstValue))
-#print (str(LastValue))
-print('Average Change: '+ str(AvarageChange))
-print('MAX: '+ str(DateMax)+':' + str(MaxValue))
-print('MIN: '+ str(DateMin)+':' + str(MinValue))
-print('Maximum Change: '+ MaxChangedate+':' + str(MaxChange))
-print('Miinimum change: '+ MinChangedate +':' + str(MinChange))
+# Printing the report on Screen
+print(Fore.RED+"Financial Analysis From "+ FirstDay+ ' to ' +LastDay)
+print("--------------------------------------------"+Style.RESET_ALL)
+print(('{:<20s}{:<10s}{:^12s}{:>1.1s}'.format('Total Months: ',str(Rowcount),'*','*')))
+print(('{:<20s}{:<10s}{:^12s}{:>1.1s}'.format('Total : ',str(Total),'*','*')))
+print(('{:<20s}{:<10.2f}{:^12s}{:>1.1s}'.format('Avarage : ',AvarageValue,'*','*')))
+print(('{:<20s}{:<10.2f}{:^12s}{:>1.1s}'.format('Avarage Change: ',AvarageChange,'*','*')))
+print(('{:<20s}{:<10s}{:^12s}{:>1.1s}'.format('MAX Profit :  ',str(DateMax) , '+'+str(MaxValue),'*')))
+print(('{:<20s}{:<10s}{:^12s}{:>1.1s}'.format('MIN Looses :  ',str(DateMin) , str(MinValue),'*')))
+print('\n')
+print(Fore.GREEN+'Greatest Increase in Profits: '+ MaxChangedate+' : ' + str(MaxChange))
+print('Greatest Decrease in Profits: '+ MinChangedate +' : ' + str(MinChange)+'\n'+Style.RESET_ALL)
 
+# 
+if (input('Do you want to save it as a TXT file? (y)es/(n)o?--->  ')=='y'):
+  print ( '\nThe TXT file was genarated in  '+ str(SavingRoot)+'\n\n' )
+  
+  sys.stdout= open(SavingRoot,"w")
+  print("Financial Analysis From "+ FirstDay+ ' to ' +LastDay)
+  print("----------------------------")
+  print(('{:<20s}{:<10s}{:^12s}{:>1.1s}'.format('Total Months: ',str(Rowcount),'*','*')))
+  print(('{:<20s}{:<10s}{:^12s}{:>1.1s}'.format('Total : ',str(Total),'*','*')))
+  print(('{:<20s}{:<10.2f}{:^12s}{:>1.1s}'.format('Avarage : ',AvarageValue,'*','*')))
+  print(('{:<20s}{:<10.2f}{:^12s}{:>1.1s}'.format('Avarage Change: ',AvarageChange,'*','*')))
+  print(('{:<20s}{:<10s}{:^12s}{:>1.1s}'.format('MAX Profit :  ',str(DateMax) , '+'+str(MaxValue),'*')))
+  print(('{:<20s}{:<10s}{:^12s}{:>1.1s}'.format('MIN Looses :  ',str(DateMin) , str(MinValue),'*')))
+
+  print('\n')
+  print('Greatest Increase in Profits: '+ MaxChangedate+' : ' + str(MaxChange))
+  print('Greatest Decrease in Profits: '+ MinChangedate +' : ' + str(MinChange)+'\n')
+
+  
+  
+  sys.stdout.close()
 
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,34 +1,99 @@
 import os
-
+import sys
 import  csv
+
+# CLearing The ScreenTerminal
 os.system('cls')
 
-csvpath = os.path.join('Resources','election_data.csv')
+# Adreess of Source data to read and destination of genarating reports
+
+SavingRoot='analysis/report.txt'
+ReadingRootFile='Resources/election_data.csv'
+
+# Just a pop msg for user
+input(' Warning!! Genarating report from (' +ReadingRootFile + ') may take few seconds. \n Please be patient ! \n Press ENTER To continue or Ctrl+Z to break.' )
+print('\n GENERATING REPORTS . . .')
+
+# Opening and Reading the Data from Source
+
+csvpath = os.path.join(ReadingRootFile)
 
 with open (csvpath) as csvfile :
+
     csvreader = csv.reader(csvfile, delimiter=',')
 
-    csv_header = next(csvreader)
+    csv_header = next(csvreader)   # Skipping the header
 
+    # Define the Variables
 
-    TotalVote=0
-    Candidate=[]
-    CandidateVote=[]
-    K=0
+    TotalVote=0           # Counters For number of Vote  
+    Candidate=[]          # List of Candidate
+    CandidateVote=[]      # List of Number of votes of each Candidate
+    
 
-    print (csv_header)
-  
+    
+    for row in csvreader:                       # Reading the file by Row
 
-    for row in csvreader:
-      if row[2] not in Candidate:
+      if row[2] not in Candidate:               
+        
+        # If the name Candidate is not in Candidate list add it and 
+        # add 1 for it's first vote in the CandidateVote List
         Candidate.append(row[2])
         CandidateVote.append(1)
       else:
+          # Find the index number of the candidate and 1 to the number of thier votes
           x = Candidate.index(row[2])
           CandidateVote[x]=1+int(CandidateVote[x])
+      # Adding up the Number of Voters
       TotalVote=TotalVote+1
 
 
-print(Candidate)
-print(CandidateVote)
+# Desiging to print the report in the screen
+
+print('\n\n'+'Election Results'+ '\n' + '-------------------------')
+print('Total Votes: '+ str(TotalVote) + '\n' 
++'------------------------')
+
+i=0  # counter for looping in the List of Candidate and Candidatevote
+
+print('{:<10s}{:>4s}{:^12s}{:>1.1s}'.format('Candidate','%','Votes',''))
+for i in range(len(Candidate)) :
+  print('{:<10s}{:>4s}{:^12s}{:>1.1s}'.format(Candidate[i],str(int(round(CandidateVote[i]*100/TotalVote))),str(CandidateVote[i]),'*'))
+
+print('------------------------')
+
+# Indexing the Max Vote from CandidateVote list And them finding the Winner from Candidate list
+print ('WINNER :  '+Candidate[CandidateVote.index(max(CandidateVote))])
+
+print('------------------------\n\n')
+# Confirming whit user to save the report file by pressing 'y'
+
+if (input('Do you want to save it as a TXT file? (y)es/(n)o?  ')=='y'):
+  print ( '\nThe TXT file was genarated in  '+ str(SavingRoot)+'\n\n' )
+
+  sys.stdout= open(SavingRoot,"w")
+
+  # Desiging to print the report in the file
+
+  print('Election Results'+ '\n' + '-------------------------')
+  print('Total Votes: '+ str(TotalVote) + '\n' +'------------------------')
+  
+  i=0
+  print('{:<10s}{:>4s}{:^12s}{:>1.1s}'.format('Candidate','%','Votes',''))
+  for i in range(len(Candidate)) :
+    print('{:<10s}{:>4s}{:^12s}{:>1.1s}'.format(Candidate[i],str(int(round(CandidateVote[i]*100/TotalVote))),str(CandidateVote[i]),'*'))
+
+  print('------------------------')
+
+  # Indexing the Max Vote from CandidateVote list And them finding the Winner from Candidate list
+  print ('WINNER :  '+Candidate[CandidateVote.index(max(CandidateVote))])
+
+  print('------------------------')
+
+
+  sys.stdout.close()
+  
+  
+# a= input('Press any key to Exit')
+
 
